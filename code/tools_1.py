4832da8f0fee1941bbe4bd710eb7253a5d8ebe5f
GithubZhiyin <LSJ2426@outlook.com>
2026-03-04 16:42:19 +0800

    Update tools_1.py

--- code/tools_1.py
+++ code/tools_1.py
@@ -1,1 +1,22 @@
+import numpy as np 
 
+## Two commonly used stacking functions： vstack（vertical） 和 hstack（horizontal）
+
+  
+
+
+corr_matrix = lambda d: np.ma.corrcoef(np.ma.masked_invalid(np.array([np.array(m).flatten() for m in d]))).data
+
+def calculate_correlation_matrix(data_list):
+    flattened_arrays = []
+    for array in data_list:
+        array = np.array(array)
+        flattened = array.flatten()
+        masked_array = np.ma.masked_invalid(flattened)
+        flattened_arrays.append(masked_array)
+    
+    stacked_arrays = np.ma.vstack(flattened_arrays)
+    correlation_matrix_masked = np.ma.corrcoef(stacked_arrays)
+    correlation_matrix = correlation_matrix_masked.data
+    
+    return correlation_matrix
